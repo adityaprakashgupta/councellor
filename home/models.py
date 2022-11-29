487954259883be71852e0a19448f3644065bbf88
Aditya Prakash Gupta <adityaprakashgupta726@gmail.com>
2022-11-29 12:19:14 +0530

    Appointments table not completed

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from accounts.models import BaseModel, User
+from .validators import group_validators
 
 # Create your models here.
 class Appointments(BaseModel):
-    client = models.ForeignKey(User, on_delete=models.CASCADE, validators=)
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- home/validators.py
+++ home/validators.py
@@ -3,6 +3,6 @@ from django.core import exceptions
 
 
 def group_validators(value):
-    if not Group.objects.filter(name=value).exists():
-        raise exceptions.ValidationError("Group does not exists.", code="group_not_found")
-    return value
+    print(value, type(value))
+    # if not Group.objects.filter(name=value).exists():
+    raise exceptions.ValidationError("Group does not exists.", code="group_not_found")
