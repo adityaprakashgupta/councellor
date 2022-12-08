7c158f25b3040b922fe599c095b19ea70def1afc
Aditya Prakash Gupta <adityaprakashgupta726@gmail.com>
2022-12-08 13:42:51 +0530

    Improved Install logic

--- counsellor/settings.py
+++ counsellor/settings.py
@@ -188,7 +188,7 @@ DJOSER = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = 'cec748e4e7e836'
-EMAIL_HOST_PASSWORD = '76bc0e64c5b3f5'
-EMAIL_PORT = '2525'
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
